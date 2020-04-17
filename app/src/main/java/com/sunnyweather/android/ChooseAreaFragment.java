12f392ac9b5230232ce1f335a76cacc88d9a3e76
杨明福 <yangminfu@hotmail.com>
2020-04-17 22:12:47 +0800

    province记录省的名字 省的代号 city记录市的名字 市的代号 province记录所属省的id county记录县的名字 weatherId记录所对应的天气 cityId记录当前县所属市的id 用Utility来解析处理返回的数据 新建布局choose_area 需要遍历省市县数据的碎片，新建chooseAreaFragment

---
+++ app/src/main/java/com/sunnyweather/android/ChooseAreaFragment.java
@@ -0,0 +1,13 @@
+package com.sunnyweather.android;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Package: com.sunnyweather.android
+ * Name: AreaFragment
+ * Author: yangmingfu
+ * Create Time：2020/4/17
+ * Description：
+ */
+public class ChooseAreaFragment extends Fragment {
+}
---
+++ app/src/main/java/db/Province.java
@@ -0,0 +1,46 @@
+package db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * Package: db
+ * Name: Province
+ * Author: yangmingfu
+ * Create Time：2020/4/13
+ * Description：
+ */
+public class Province extends LitePalSupport {
+
+
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
