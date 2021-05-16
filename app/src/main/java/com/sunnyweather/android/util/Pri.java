ffe180f457b72fdc6ffe10ae98a8bc8bc483d885
yangmf <yangminfu@hotmail.com>
2021-05-16 14:13:34 +0800

    分支修改代码02

--- app/src/main/java/com/sunnyweather/android/util/Pri.java
+++ app/src/main/java/com/sunnyweather/android/util/Pri.java
@@ -1,4 +1,13 @@
 package com.sunnyweather.android.util;
 
 public class Pri {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
